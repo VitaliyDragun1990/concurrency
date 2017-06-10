733c644a85ecbb299e9f303fab0a26758542b71a
Vitaliy Dragun <vdrag00n90@gmail.com>
2017-06-10 20:17:07 +0300

    Little test

---
+++ Concurrency/src/tkach/tutorial/Test1.java
@@ -0,0 +1,14 @@
+package tkach.tutorial;
+
+public class Test1 {
+
+	/**
+	 * Some test
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
